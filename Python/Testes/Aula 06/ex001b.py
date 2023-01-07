648dae3ab48188d118c94d2625f30484a0d01727
Vhyctorugo <dev.vitorsobrinho@outlook.com>
2023-01-07 13:53:19 -0300

    Inclusão de 'nova pergunta' para o dado do usuário

--- Python/Testes/Aula 06/ex001b.py
+++ Python/Testes/Aula 06/ex001b.py
@@ -3,3 +3,4 @@ print('Ele é um número?', n.isnumeric())
 print('Ele é um alfabeto?', n.isalpha())
 print('Ele é uma letra maiuscula?', n.isupper())
 print('Ele é uma letra minúscula?', n.islower())
+
