766cfc7afabb1f16175ca50f2ccf3bc12513d893
Vhyctorugo <dev.vitorsobrinho@outlook.com>
2023-01-07 13:42:42 -0300

    Commit inicial

---
+++ Python/Testes/Aula 06/ex001b.py
@@ -0,0 +1,5 @@
+n = input('Digite um valor: ') 
+print('Ele é um número?', n.isnumeric())
+print('Ele é um alfabeto?', n.isalpha())
+print('Ele é uma letra maiuscula?', n.isupper())
+print('Ele é uma letra minúscula?', n.islower())
