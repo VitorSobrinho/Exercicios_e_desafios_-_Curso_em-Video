2a7347a55b1e87fa1ae8c12af2811d1c8fc314de
Vhyctorugo <dev.vitorsobrinho@outlook.com>
2023-01-07 15:11:12 -0300

    Exercício resolvido após a aula

--- Python/Testes/Aula 06/ex001b.py
+++ Python/Testes/Aula 06/ex001b.py
@@ -1,6 +1,11 @@
 n = input('Digite um valor: ') 
 print('Ele é um número?', n.isnumeric())
 print('Ele é um alfabeto?', n.isalpha())
-print('Ele é uma letra maiuscula?', n.isupper())
-print('Ele é uma letra minúscula?', n.islower())
+print('Ele tem letras e números?', n.isalnum())
+print('Ele é apenas com letras maiusculas?', n.isupper())
+print('Ele é apenas com letras minúsculas?', n.islower())
+print('Qual é o tipo primitivo deste valor', type(n))
+print('Ele é apenas espaços?', n.isspace())
+print('Ela está capitalizada?', n.istitle()) #Perguntando se está com a primeira letra em maiúsculo.
+
 
